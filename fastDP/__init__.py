77216a4bb15ee961de09af0ac8e1024ed3936026
Zhiqi Bu <woodyx218@gmail.com>
2024-02-09 15:33:37 +0800

    Update __init__.py

--- fastDP/__init__.py
+++ fastDP/__init__.py
@@ -3,4 +3,4 @@ from .privacy_engine import PrivacyEngine
 from .privacy_engine_dist_stage23 import PrivacyEngine_Distributed_Stage_2_and_3
 #from .privacy_engine_dist_extending import PrivacyEngine_Distributed_extending
 from .supported_differentially_private_layers import replace_Linear, replace_Conv2d, replace_LayerNorm,replace_Embedding,replace_GELUConv1D,replace_transformersConv1D
-__version__ = '2.1.0'
+__version__ = '2.0.0'
