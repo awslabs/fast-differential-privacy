aca8cd6f35b21fc29b10d4128584697a7bdc1b4c
Zhiqi Bu <woodyx218@gmail.com>
2024-02-12 15:50:35 +0800

    Update __init__.py

--- fastDP/__init__.py
+++ fastDP/__init__.py
@@ -1,6 +1,6 @@
 from . import lora_utils
 from .privacy_engine import PrivacyEngine
 from .privacy_engine_dist_stage23 import PrivacyEngine_Distributed_Stage_2_and_3
-#from .privacy_engine_dist_extending import PrivacyEngine_Distributed_extending
-#from .supported_differentially_private_layers import *
+from .privacy_engine_dist_extending import PrivacyEngine_Distributed_extending
+from .supported_differentially_private_layers import *
 __version__ = '2.0.0'
