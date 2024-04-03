454f6f2bb85ad65d7d3fc8bce444c6fee63f0f6f
Zhiqi Bu <woodyx218@gmail.com>
2024-04-03 09:23:37 -0400

    Update run_ZERO1.sh

--- examples/table2text/run_ZERO1.sh
+++ examples/table2text/run_ZERO1.sh
@@ -59,7 +59,7 @@ deepspeed table2text/run_language_modeling.py --deepspeed_config ${deepspeed_con
   --logging_dir ${output_dir} --logging_steps -1 \
   --seed 0 \
   --dataloader_num_workers 2 \
-  --eval_steps 100 --eval_epochs 999 --max_eval_batches 100 --evaluation_strategy epoch --evaluate_before_training "no" \
+  --eval_steps -1 --eval_epochs 999 --max_eval_batches 100 --evaluation_strategy epoch --evaluate_before_training "no" \
   --evaluate_during_training "no" --per_device_eval_batch_size 10 \
   --max_generations 9223372036854775807 --max_generations_train 10 --max_generations_valid 9223372036854775807 \
   --max_train_examples 9223372036854775807 --max_valid_examples 9223372036854775807 --max_eval_examples 9223372036854775807 \
